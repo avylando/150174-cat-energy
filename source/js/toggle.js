7c42e6e7072e45857ff9acc3a0d7b350b0f5526c
Anatoly Dolgov <anatoldd@list.ru>
2018-01-28 00:34:59 +0600

    tablet ready

--- source/js/toggle.js
+++ source/js/toggle.js
@@ -1,11 +1,14 @@
 'use strict';
 
-(function() {
+(function () {
 
   // Mobile menu
 
   var nav = document.querySelector('.header__wrapper');
   var burger = nav.querySelector('.header__burger');
+  var docWidth = document.body.clientWidth;
+  var tabletWidth = 768;
+
 
   nav.classList.remove('header__wrapper--nojs');
 
@@ -24,17 +27,69 @@
     var btnBefore = progress.querySelector('.demo__progress-button--before');
     var btnAfter = progress.querySelector('.demo__progress-button--after');
 
-    btnBefore.addEventListener('click', function() {
+    btnBefore.addEventListener('click', function () {
       if (progress.classList.contains('demo__progress--after')) {
         progress.classList.remove('demo__progress--after');
       }
     })
 
-    btnAfter.addEventListener('click', function() {
+    btnAfter.addEventListener('click', function () {
       if (!progress.classList.contains('demo__progress--after')) {
         progress.classList.add('demo__progress--after');
       }
     })
+
+    if (docWidth >= tabletWidth) {
+      var imgBefore = progress.querySelector('.demo__illustration--before');
+      var scale = progress.querySelector('.demo__progress-bar');
+      var toggle = scale.querySelector('.demo__progress-toggle');
+
+      var scaleWidth = 427;
+      var percentScale = 4.27;
+      var minScaleValue = 0;
+      var maxScaleValue = 100;
+
+      toggle.addEventListener('mousedown', function (evt) {
+        evt.preventDefault();
+
+        var startCoords = {
+          x: evt.clientX
+        }
+
+        var pinMouseMoveHandler = function (moveEvt) {
+          moveEvt.preventDefault();
+
+          var shift = {
+            x: startCoords.x - moveEvt.clientX
+          };
+
+          startCoords = {
+            x: moveEvt.clientX
+          };
+
+          toggle.style.left = ((toggle.offsetLeft - shift.x) / percentScale) + '%';
+          imgBefore.style.width = (100 - Math.floor((toggle.offsetLeft - shift.x) / percentScale)) + '%';
+
+          var toggleOffsetPercent = parseInt(toggle.style.left, 10);
+
+          if (toggleOffsetPercent <= minScaleValue) {
+            toggle.style.left = minScaleValue + '%';
+          } else if (toggleOffsetPercent >= maxScaleValue) {
+            toggle.style.left = maxScaleValue + '%';
+          }
+        }
+
+        var pinMouseUpHandler = function () {
+          document.removeEventListener('mousemove', pinMouseMoveHandler);
+          document.removeEventListener('mouseup', pinMouseUpHandler);
+        }
+
+        document.addEventListener('mousemove', pinMouseMoveHandler);
+        document.addEventListener('mouseup', pinMouseUpHandler);
+      })
+    }
   }
+
+
 
 })();
